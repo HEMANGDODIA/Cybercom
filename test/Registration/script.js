17ca31bd73851a89250376bd4c2fef21e4e787da
Hemang Dodiya <51086213+HEMANGDODIA@users.noreply.github.com>
2021-01-21 14:52:17 +0530

    dashboard n other file with change added

--- test/Registration/script.js
+++ test/Registration/script.js
@@ -23,5 +23,27 @@ function addElement() {
     localStorage.setItem("array", JSON.stringify(array));
 
     alert(sname + " " + semail + " " + bdate + " added at index " + array.length);
-
 };
+
+function login() {
+    event.preventDefault();
+    console.log("login function called...");
+    var sEmail = document.getElementById('uemail').value;
+    var sPwd = document.getElementById('upassword').value;
+    console.log(sEmail);
+    console.log(sPwd);
+
+    if (localStorage.getItem('array')) {
+        array = JSON.parse(localStorage.getItem('array'));
+    }
+    console.log(array.length);
+    for (i = 0; i < array.length; i++) {
+        if (!sEmail.localeCompare(array[i].email) && !sPwd.localeCompare(array[i].password)) {
+            console.log("Logged in...");
+
+            // window.location.replace("http://www.w3schools.com");
+            window.location.href = '../Dashboard.html';
+        }
+        console.log("incorrect uername & password...");
+    }
+}
